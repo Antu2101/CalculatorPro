2a57c90f4520a52ea08c069aa0eb04dd251e2f3c
Antu2101 <anu.ch.abes@gmail.com>
2023-08-09 14:56:51 +0530

    adding cpp file

---
+++ Calculator/Main.cpp
@@ -0,0 +1,15 @@
+ namespace Study
+ {
+    class calculate
+    {
+        public:
+            calculate()
+            {
+                a=0;
+                b=0;
+            }
+        private:
+            int a;
+            int b;
+    };
+ }
